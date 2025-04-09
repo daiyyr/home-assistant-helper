7790c13a8bb565a8e3afd040ef13f58db360eebf
Teemo <eru7921129@gmail.com>
2025-04-09 22:28:10 +1200

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # granular-dynamic-dns
 Regularly update R53 records to point to local machine(s) public IP. Each machine use a different AWS user which can only update a specific DNS
+
+
+# New machine setup
+- Update workflows/deploy-dns-updaters.yaml, add new machine nick name to MachineNameList e.g. home1,machine2,machine3. The Github workflow should be triggered to create the new user with policies
+- Go to aws console to get AWS Secret Access Key and AWS Access Key ID for that user
+- SSH to the new machine, install aws cli and configure AWS CLI with the above AWS credentials. Depending on the OS, you may need to use the relevant package manager to install the AWS CLI. Home Assistant Operating System for Raspberry Pi is based on Alpine Linux, so we use apk:
+
+```
+apk add aws-cli
+aws config
+```
+
+- copy the folder scripts/dynamic-dns to /opt/
+- add a crontab record:
+
+```
+crontab -e
+
+* * * * * /opt/dynamic-dns/update-dns.sh >> /opt/dynamic-dns/update-dns.log 2>&1
+```
---
+++ scripts/dynamic-dns/update-dns.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# update MachineNickName for each machine
+R53HostedZoneId="Z06958611JDYVCG41K93R"
+MachineNickName="home"
+
+IP_FILE="/opt/dynamic-dns/previous-ip.txt"
+mkdir -p "$(dirname "$IP_FILE")"
+
+# Read the previous IP from the file
+if [ -f "$IP_FILE" ]; then
+  PREVIOUS_IP=$(cat "$IP_FILE")
+else
+  PREVIOUS_IP=""
+fi
+
+# Fetch the public IP
+PUBLIC_IP=$(curl -s https://checkip.amazonaws.com)
+
+if [ "$PUBLIC_IP" != "$PREVIOUS_IP" ]; then
+  echo "Public IP has changed. Updating Route 53 record..."
+    
+    # if R53HostedZoneId exists, update Route 53 record
+    if [ -z "$R53HostedZoneId" ]; then
+        echo "R53HostedZoneId is unset"
+    else
+        DOMAIN_NAME=`aws route53 get-hosted-zone --id ${R53HostedZoneId} --query 'HostedZone.Name' --output text | sed 's/.$//'`
+        URL=${MachineNickName}.$DOMAIN_NAME
+        aws route53 change-resource-record-sets --hosted-zone-id ${R53HostedZoneId} --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'"$URL"'","Type":"A","TTL":10,"ResourceRecords":[{"Value":"'"$PUBLIC_IP"'"}]}}]}'
+    fi
+
+  # Save the new IP for future comparisons
+  echo $PUBLIC_IP > "$IP_FILE"
+
+else
+  echo "Public IP has not changed."
+fi
