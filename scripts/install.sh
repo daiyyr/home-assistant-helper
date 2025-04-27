ff6680d5b13da9444c58fc3c68ea81422abe3c41
Teemo <eru7921129@gmail.com>
2025-04-28 01:32:51 +1200

    letsencrypt

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,10 +11,11 @@ if [ -z "$1" ]; then
 fi
 
 MACHINE_NICKNAME="$1"
+DOMEAIN_NAME="the-alchemist.link"
 
 # Install required packages. 
 # Depending on the OS, you may need to use the relevant package manager to install the AWS CLI. Home Assistant Operating System for Raspberry Pi is based on Alpine Linux, so we use apk here
-apk add git cronie openrc aws-cli curl
+apk add git cronie openrc aws-cli curl certbot certbot-dns-route53
 
 # Configure AWS CLI
 aws configure
@@ -23,7 +24,12 @@ aws configure
 # Set up git and machine nickname
 echo "$MACHINE_NICKNAME" > /opt/machine_nickname.txt
 git config --global user.name "$MACHINE_NICKNAME"
-git config --global user.email "$MACHINE_NICKNAME@the-alchemist.com"
+git config --global user.email "$MACHINE_NICKNAME@$DOMEAIN_NAME"
+
+# Create certificate
+if [ ! -f "/etc/letsencrypt/live/$MACHINE_NICKNAME.$DOMEAIN_NAME/fullchain.pem" ]; then
+    certbot certonly --dns-route53 -d $MACHINE_NICKNAME.$DOMEAIN_NAME --non-interactive --agree-tos --dns-route53-propagation-seconds 30
+fi
 
 # Clone the helper repo
 if [ -d "/opt/home-assistant-helper" ]; then
@@ -46,6 +52,9 @@ if ! grep -q "backup-to-s3.sh" /etc/crontabs/root; then
 fi
 if ! grep -q "push-to-github.sh" /etc/crontabs/root; then
     echo "*/3 * * * * /opt/home-assistant-helper/scripts/push-to-github.sh >> /var/log/github-backup.log 2>&1" >> /etc/crontabs/root
+fi
+if ! grep -q "certbot" /etc/crontabs/root; then
+    echo "0 2 * * * certbot renew --quiet" >> /etc/crontabs/root
 fi
 
 # Start cron daemon
