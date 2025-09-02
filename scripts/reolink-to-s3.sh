d152adef5716b1c0056beb897fe9ddcdbe8ea0c3
Teemo <eru7921129@gmail.com>
2025-09-02 13:40:26 +1200

    debug

--- scripts/reolink-to-s3.sh
+++ scripts/reolink-to-s3.sh
@@ -41,7 +41,8 @@ do
             sync "$ZIP_PATH"
             if aws s3 mv --no-progress --only-show-errors -- "$ZIP_PATH" "$S3_ZIP_PATH"; then
                 # on success, remove TEMP_FILE
-                rm -f -- "$TEMP_FILE"
+                # rm -f -- "$TEMP_FILE"
+                echo "succeed"
             else
                 echo "$(date +%Y%m%d_%H%M%S): Upload failed for $ZIP_PATH -> $S3_ZIP_PATH, keeping files"
             fi
